6585cbccd8de41a7c7401e5392f1fb0688ed8751
Assstarte <krivoguzov35@gmail.com>
2019-01-09 21:46:11 +0200

    GraphQL On Client | Implemented Chat Rooms obtain method

--- chat-fe/src/actions/f_msgs_and_rooms.js
+++ chat-fe/src/actions/f_msgs_and_rooms.js
@@ -1,28 +1,34 @@
 import { FETCH_MESSAGES, FETCH_CHATROOMS, ERROR_OCCURRED } from "./types";
 
+import { GraphQLClient } from "graphql-request";
+
+const gql = new GraphQLClient("http://localhost:3030/api", { headers: {} });
+
 export const exec_fetch_rooms = () => dispatch => {
-  fetch("/rooms", {
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json"
-    },
-    method: "GET"
-  }).then(res =>
-    res
-      .json()
-      .then(data =>
-        dispatch({
-          type: FETCH_CHATROOMS,
-          payload: data
-        })
-      )
-      .catch(res => {
-        dispatch({
-          type: ERROR_OCCURRED,
-          payload: res
-        });
-      })
-  );
+  gql
+    .request(
+      `query getChatRooms {
+    getChatRooms {
+      id
+      name
+      owner_name
+    }
+  }`,
+      {}
+    )
+    .then(data => {
+      console.dir(`DATA========= ${data}`);
+      dispatch({
+        type: FETCH_CHATROOMS,
+        payload: data.getChatRooms
+      });
+    })
+    .catch(res => {
+      dispatch({
+        type: ERROR_OCCURRED,
+        payload: res
+      });
+    });
 };
 
 export const exec_fetch_messages = roomId => dispatch => {
